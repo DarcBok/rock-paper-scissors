3d09cc02b71795734e74d9f699b8ac82cae170b2
DarcBok <105783009+DarcBok@users.noreply.github.com>
2023-01-29 21:30:43 +1100

    Add Game Over condition

--- main.js
+++ main.js
@@ -3,13 +3,21 @@ const WIN = 1, TIE = 0, LOSE = -1;
 const buttons = document.querySelector('.icons').querySelectorAll('button');
 const playerScoreText = document.querySelector('.player');
 const compScoreText = document.querySelector('.computer');
+const compChoiceText = document.querySelector('.choice');
+const playAgain = document.querySelector(`.play-again`);
+const canvas = document.querySelector(`#my-canvas`);
 const message = document.querySelector('.message');
-
-console.log(buttons);
+let timeout;
 
 let playerScore = 0;
 let compScore = 0;
+let confettiSettings = { target: 'my-canvas' };
+let confetti = new ConfettiGenerator(confettiSettings);
 
+function updateScores() {
+    playerScoreText.textContent = `Player score: ${playerScore}`;
+    compScoreText.textContent = `Computer score: ${compScore}`;
+}
 
 function playRound(button) {
     if (button.srcElement.classList.contains("rock")) round("rock");
@@ -34,7 +42,6 @@ function randomChoice() {
 
 function round(playerChoice) {
     let compChoice = randomChoice();
-    console.log(compChoice);
     if (playerChoice == "rock" && compChoice == "scissors") {
         playerScore++;
         showOnScreen("rock", "scissors", WIN);
@@ -47,7 +54,7 @@ function round(playerChoice) {
         showOnScreen("rock", "rock", TIE);
     }
     else if (playerChoice == "paper" && compChoice == "scissors") {
-        compScore;
+        compScore++;
         showOnScreen("paper", "scissors", LOSE);
     }
     else if (playerChoice == "paper" && compChoice == "paper") {
@@ -91,21 +98,65 @@ function clearButton(button) {
     button.classList.remove("lose");
 }
 
-function showOnScreen(playerChoice, compChoice, outcome) {
+function clearRound() {
     buttons.forEach(clearButton);
+    compChoiceText.textContent = `Choose your weapon!`;
+    message.textContent = `First to 5 wins!`;   
+}
+
+function resetScreen() {
+    clearRound();
+    playerScore = 0;
+    compScore = 0;
+    updateScores();
+    canvas.classList.remove('active');
+    playAgain.classList.remove('active');
+}
+
+playAgain.addEventListener("click", resetScreen);
+
+function gameOver(winner) {
+    if (winner == "player") {
+        message.textContent = "You reached 5 points! You win the game!";
+        playAgain.classList.add('active');
+        canvas.classList.add('active');
+        confetti.render();
+    } else {
+        message.textContent = "Computer reached 5 points! You lose!";
+        playAgain.classList.add('active');
+    }
+}
+
+function showOnScreen(playerChoice, compChoice, outcome) {
+    // clear previous choices
+    clearTimeout(timeout);
+    buttons.forEach(clearButton); 
+
     lightUp(playerChoice, outcome);
     lightUp(compChoice, -outcome);
 
     // capitalise playerChoice and compChoice 
     playerChoice = playerChoice[0].toUpperCase() + playerChoice.slice(1);
     compChoice = compChoice[0].toUpperCase() + compChoice.slice(1);
+
+    compChoiceText.textContent = `I pick ${compChoice}!`;
+    updateScores();
     if (outcome == WIN) {
-        playerScoreText.textContent = `Player score: ${playerScore}`;
         message.textContent = `${playerChoice} beats ${compChoice}! You win the round!`;
     } else if (outcome == LOSE) {
-        compScoreText.textContent = `Computer score: ${compScore}`;
         message.textContent = `${playerChoice} loses to ${compChoice}! You lose the round!`;
     } else {
         message.textContent = `Both choose ${playerChoice}! You tie the round!`;
     }
+
+    if (playerScore == 5) {
+        gameOver("player");
+        return;
+    }
+    if (compScore == 5) {
+        gameOver("comp");
+        return;
+    }
+
+    timeout = setTimeout(clearRound, 3000);
 }
